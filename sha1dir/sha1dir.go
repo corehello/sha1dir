5d3e163971c553d43d2b5d2682cd3f45cf134351
corehello <corehello@gmail.com>
2017-03-14 11:52:59 +0800

    fix typo

--- sha1dir/sha1dir.go
+++ sha1dir/sha1dir.go
@@ -15,7 +15,7 @@ import (
 var wg sync.WaitGroup
 
 func sha1file(path string, info os.FileInfo, err error) error {
-	if !match(defaultblacklist, path) {
+	if !match(blacklist, path) {
 		switch mode := info.Mode(); {
 		case mode.IsRegular():
 			wg.Add(1)
@@ -25,7 +25,8 @@ func sha1file(path string, info os.FileInfo, err error) error {
 					fmt.Println(err)
 				}
 				sum := sha1.Sum(content)
-				o := fmt.Sprintf("%s, %x, %d", filepath.Abs(path), sum, info.Size())
+        abspath,_ := filepath.Abs(path)
+				o := fmt.Sprintf("%s, %x, %d", abspath, sum, info.Size())
 				output <- o
 				//fix the data lost for buffered channel
 				time.Sleep(1 * time.Millisecond)
@@ -67,12 +68,12 @@ var blacklist []string
 var output = make(chan string, 31)
 
 func Run(rootpath string, filter []string, outputfile string) {
-	ex, err = PathExists(rootpath)
+	_, err := PathExists(rootpath)
 	if err != nil {
-		fmt.Println("The given path: " + rootpath + " is not exited on disk!!")
+		fmt.Println("The given path: " + rootpath + " is not existed on disk!!")
 		return
 	}
-	for fil := range filter {
+	for _,fil := range filter {
 		blacklist = append(blacklist, fil)
 	}
 	runtime.GOMAXPROCS(4)
